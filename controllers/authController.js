7d604b3f51e5ff34d58e6d6e46aadbcb1d3c7224
8pro <mechackkobius@gmail.com>
2025-09-10 15:22:18 +0300

    Create a protect route middleware to help catch unauthenticated use

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,35 @@ exports.login = catchAsync(async (req, res, next) => {
   });
 });
 
+// Protected route example
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(new AppError("You are not logged in! Please log in.", 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const currentUser = await User.findUnique({ where: { id: decoded.id } });
+    if (!currentUser) {
+      return next(
+        new AppError("The user belonging to this token no longer exists.", 401)
+      );
+    }
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    return next(new AppError("Invalid token. Please log in again.", 401));
+  }
+});
+
 // const { OAuth2Client } = require("google-auth-library"); // For Google auth
 // const fetch = require('node-fetch');
 
